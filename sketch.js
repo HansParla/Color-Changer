b827e79727bb650cd54d737b2773e0ff8558ca4a
HansParla <88782440+HansParla@users.noreply.github.com>
2021-08-28 13:06:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+
+if (keyIsDown(UP_ARROW)) {
+  background('red');
+
+}
+
+if (keyIsDown(DOWN_ARROW)) {
+  background('green');
+
+}  
+
+if (keyIsDown(LEFT_ARROW)) {
+  background("yellow");
+
+}
+
+if (keyIsDown(RIGHT_ARROW)) {
+  background("blue");
+
+}
+
+}
+
+
+
+
+
